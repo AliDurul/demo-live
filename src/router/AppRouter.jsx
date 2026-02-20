d01307ee994640572249d891d4ff91f04ea75d6d
AliDurul <alidrl26@gmail.com>
2026-02-20 11:15:18 +0200

    up

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Feed from "../pages/Feed";
-import Profile from "../pages/profile";
+import Profile from "../pages/Profile";
 import Friends from "../pages/Friends";
 import Messages from "../pages/Messages";
 import RootLayout from "../components/RootLayout";
